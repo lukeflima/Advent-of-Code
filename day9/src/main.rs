b315fb1090b52e0ff28c490f31ce6bc8291532ef
unknown <lukeflima@gmail.com>
2021-12-09 20:41:36 -0300

    Added day9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,7 +7,8 @@ fn part1() {
         .trim()
         .split('\n')
         .map(|x| {
-            x.chars()
+            x.trim()
+                .chars()
                 .map(|x| parse_number::<usize>(&x.to_string()))
                 .collect()
         })
@@ -45,7 +46,69 @@ struct Cell {
     visited: bool,
 }
 
-fn part2() {}
+fn visit(
+    basins: &mut Vec<Vec<usize>>,
+    heightmap: &mut Vec<Vec<Cell>>,
+    i: usize,
+    j: usize,
+    basin_index: usize,
+) {
+    if heightmap[i][j].visited || heightmap[i][j].value == 9 {
+        return;
+    }
+    if basin_index == 0 {
+        basins.push(Default::default());
+    }
+    basins.last_mut().unwrap().push(heightmap[i][j].value);
+    heightmap[i][j].visited = true;
+    if i != 0 {
+        visit(basins, heightmap, i - 1, j, basin_index + 1);
+    }
+    if i < heightmap.len() - 1 {
+        visit(basins, heightmap, i + 1, j, basin_index + 1);
+    }
+    if j != 0 {
+        visit(basins, heightmap, i, j - 1, basin_index + 1);
+    }
+    if j < heightmap[i].len() - 1 {
+        visit(basins, heightmap, i, j + 1, basin_index + 1);
+    }
+}
+
+fn part2() {
+    let mut heightmap: Vec<Vec<Cell>> = std::fs::read_to_string("input")
+        .unwrap()
+        .as_mut_str()
+        .trim()
+        .split('\n')
+        .map(|x| {
+            x.trim()
+                .chars()
+                .map(|x| Cell {
+                    value: parse_number::<usize>(&x.to_string()),
+                    visited: false,
+                })
+                .collect()
+        })
+        .collect();
+    let mut basins: Vec<Vec<usize>> = Default::default();
+    for i in 0..heightmap.len() {
+        for j in 0..heightmap[i].len() {
+            visit(&mut basins, &mut heightmap, i, j, 0);
+        }
+    }
+    basins.sort_by(|a, b| {
+        let a_size = a.len();
+        let b_size = b.len();
+        a_size.cmp(&b_size)
+    });
+    let res: usize = basins
+        .iter()
+        .skip(basins.len() - 3)
+        .map(|x| x.len())
+        .product();
+    println!("part2 {}", res);
+}
 
 fn main() {
     part1();
