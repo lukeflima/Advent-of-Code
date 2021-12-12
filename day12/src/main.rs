93d1ae80ed2ff7a60870c06dd798cfc371dd5d84
unknown <lukeflima@gmail.com>
2021-12-12 11:40:17 -0300

    Added day12

---
+++ day12/src/main.rs
@@ -0,0 +1,96 @@
+use std::collections::HashMap;
+
+fn part1() {
+    let mut graph: HashMap<String, Vec<String>> = Default::default();
+    {
+        let nodes_conn: Vec<Vec<String>> = std::fs::read_to_string("input")
+            .unwrap()
+            .as_mut_str()
+            .trim()
+            .split('\n')
+            .map(|x| x.trim().split('-').map(String::from).collect())
+            .collect();
+        for conn in nodes_conn {
+            if !graph.contains_key(&conn[0]) {
+                graph.insert(conn[0].clone(), Default::default());
+            }
+            if !graph.contains_key(&conn[1]) {
+                graph.insert(conn[1].clone(), Default::default());
+            }
+            graph.get_mut(&conn[0]).unwrap().push(conn[1].clone());
+            graph.get_mut(&conn[1]).unwrap().push(conn[0].clone());
+        }
+    }
+    let mut paths: Vec<Vec<String>> = Default::default();
+    let mut unfinesh_paths: Vec<Vec<String>> = vec![vec![String::from("start")]];
+
+    while !unfinesh_paths.is_empty() {
+        let path = unfinesh_paths.pop().unwrap();
+        if path.last().unwrap() == "end" {
+            paths.push(path);
+            continue;
+        }
+        for node in graph.get(&path[path.len() - 1]).unwrap() {
+            if node.chars().next().unwrap().is_uppercase() || !path.contains(node) {
+                unfinesh_paths.push(path.clone());
+                unfinesh_paths.last_mut().unwrap().push(node.clone());
+            }
+        }
+    }
+
+    println!("part1 {}", paths.len());
+}
+
+fn part2() {
+    let mut graph: HashMap<String, Vec<String>> = Default::default();
+    {
+        let nodes_conn: Vec<Vec<String>> = std::fs::read_to_string("input")
+            .unwrap()
+            .as_mut_str()
+            .trim()
+            .split('\n')
+            .map(|x| x.trim().split('-').map(String::from).collect())
+            .collect();
+        for conn in nodes_conn {
+            if !graph.contains_key(&conn[0]) {
+                graph.insert(conn[0].clone(), Default::default());
+            }
+            if !graph.contains_key(&conn[1]) {
+                graph.insert(conn[1].clone(), Default::default());
+            }
+            graph.get_mut(&conn[0]).unwrap().push(conn[1].clone());
+            graph.get_mut(&conn[1]).unwrap().push(conn[0].clone());
+        }
+    }
+    let mut paths: Vec<Vec<String>> = Default::default();
+    let mut unfinesh_paths: Vec<(Vec<String>, bool)> = vec![(vec![String::from("start")], false)];
+
+    while !unfinesh_paths.is_empty() {
+        let (path, small_cave_revisit) = unfinesh_paths.pop().unwrap();
+        if path.last().unwrap() == "end" {
+            paths.push(path);
+            continue;
+        }
+        for node in graph.get(&path[path.len() - 1]).unwrap() {
+            if node == "start" {
+                continue;
+            }
+            let is_upper = node.chars().next().unwrap().is_uppercase();
+            if !is_upper && !small_cave_revisit && path.iter().filter(|&x| *x == *node).count() == 1
+            {
+                unfinesh_paths.push((path.clone(), true));
+            } else if is_upper || !path.contains(node) {
+                unfinesh_paths.push((path.clone(), small_cave_revisit));
+            } else {
+                continue;
+            }
+            unfinesh_paths.last_mut().unwrap().0.push(node.clone());
+        }
+    }
+    println!("part2 {}", paths.len());
+}
+
+fn main() {
+    part1();
+    part2();
+}
