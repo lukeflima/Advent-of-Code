1c7c1cd06b864bbd6e467bf742ad358210923672
Lucas Lima <lucasflima25@gmail.com>
2021-12-18 21:37:25 -0300

    Added day16 part2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,3 @@
-use utils::parse_number;
-
 fn to_binary(c: char) -> &'static str {
     match c {
         '0' => "0000",
@@ -22,15 +20,8 @@ fn to_binary(c: char) -> &'static str {
     }
 }
 
-#[derive(Debug)]
-struct Packet {
-    version: usize,
-    type_id: usize,
-    // size: usize,
-}
-
-fn parse(bits: &Vec<char>) -> Vec<Packet> {
-    let mut packets: Vec<Packet> = Default::default();
+fn parse_part1(bits: &[char]) -> usize {
+    let mut sum = 0_usize;
     let mut i = 0_usize;
     while i < bits.len() - 6 {
         let version = usize::from_str_radix(&bits[i..i + 3].iter().collect::<String>(), 2).unwrap();
@@ -52,9 +43,9 @@ fn parse(bits: &Vec<char>) -> Vec<Packet> {
                 }
             }
         };
-        packets.push(Packet { version, type_id })
+        sum += version;
     }
-    packets
+    sum
 }
 
 fn part1() {
@@ -65,13 +56,203 @@ fn part1() {
         .map(str::chars)
         .flatten()
         .collect();
-    let packets = parse(&bits);
-    // println!("{:?}", packets);
-    let sum_versions: usize = packets.iter().map(|x| x.version).sum();
+    let sum_versions = parse_part1(&bits);
     println!("part1 {}", sum_versions);
 }
 
-fn part2() {}
+#[derive(Debug)]
+enum PacketType {
+    Sum,
+    Product,
+    Minimun,
+    Maximun,
+    Literal,
+    GreaterThen,
+    LesserThen,
+    EqualThan,
+    Unknown,
+}
+
+impl Default for PacketType {
+    fn default() -> Self {
+        Self::Unknown
+    }
+}
+impl TryFrom<usize> for PacketType {
+    type Error = ();
+
+    fn try_from(v: usize) -> Result<Self, Self::Error> {
+        match v {
+            0 => Ok(PacketType::Sum),
+            1 => Ok(PacketType::Product),
+            2 => Ok(PacketType::Minimun),
+            3 => Ok(PacketType::Maximun),
+            4 => Ok(PacketType::Literal),
+            5 => Ok(PacketType::GreaterThen),
+            6 => Ok(PacketType::LesserThen),
+            7 => Ok(PacketType::EqualThan),
+            _ => Ok(PacketType::Unknown),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Packet {
+    #[allow(dead_code)]
+    version: usize,
+    type_id: PacketType,
+    value: usize,
+    bit_size: usize,
+    subpacket: Vec<Packet>,
+}
+
+fn parse_packet(bits: &[char]) -> Packet {
+    let mut count = 0_usize;
+
+    let mut packet = Packet {
+        version: usize::from_str_radix(&bits[count..count + 3].iter().collect::<String>(), 2)
+            .unwrap(),
+        ..Default::default()
+    };
+    count += 3;
+    packet.type_id = usize::from_str_radix(&bits[count..count + 3].iter().collect::<String>(), 2)
+        .unwrap()
+        .try_into()
+        .unwrap();
+    count += 3;
+    match packet.type_id {
+        PacketType::Literal => {
+            let mut value = String::default();
+            while bits[count] == '1' {
+                value += &bits[count + 1..count + 5].iter().collect::<String>();
+                count += 5;
+            }
+            value += &bits[count + 1..count + 5].iter().collect::<String>();
+            count += 5;
+            let value = usize::from_str_radix(&value, 2).unwrap();
+            packet.value = value;
+        }
+        _ => {
+            if bits[count] == '1' {
+                let size = bits[count + 1..count + 12].iter().collect::<String>();
+                let size = usize::from_str_radix(&size, 2).unwrap();
+                count += 12;
+                for _ in 0..size {
+                    let subpacket = parse_packet(&bits[count..]);
+                    count += subpacket.bit_size;
+                    packet.subpacket.push(subpacket);
+                }
+            } else {
+                let size = bits[count + 1..count + 16].iter().collect::<String>();
+                let mut size = i64::from_str_radix(&size, 2).unwrap();
+                count += 16;
+                while size > 0 {
+                    let subpacket = parse_packet(&bits[count..]);
+                    count += subpacket.bit_size;
+                    size -= subpacket.bit_size as i64;
+                    packet.subpacket.push(subpacket);
+                }
+            }
+        }
+    };
+    packet.bit_size = count;
+    packet
+}
+
+fn solve_packet(packet: &Packet) -> Packet {
+    Packet {
+        value: match &packet.type_id {
+            PacketType::Sum => packet
+                .subpacket
+                .iter()
+                .map(solve_packet)
+                .map(|p| p.value)
+                .sum(),
+            PacketType::Product => packet
+                .subpacket
+                .iter()
+                .map(solve_packet)
+                .map(|p| p.value)
+                .product(),
+            PacketType::Minimun => packet
+                .subpacket
+                .iter()
+                .map(solve_packet)
+                .map(|p| p.value)
+                .min()
+                .unwrap(),
+            PacketType::Maximun => packet
+                .subpacket
+                .iter()
+                .map(solve_packet)
+                .map(|p| p.value)
+                .max()
+                .unwrap(),
+            PacketType::Literal => packet.value,
+            PacketType::GreaterThen => {
+                let [x, y]: [usize; 2] = packet
+                    .subpacket
+                    .iter()
+                    .map(solve_packet)
+                    .map(|p| p.value)
+                    .collect::<Vec<usize>>()
+                    .try_into()
+                    .unwrap();
+                if x > y {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketType::LesserThen => {
+                let [x, y]: [usize; 2] = packet
+                    .subpacket
+                    .iter()
+                    .map(solve_packet)
+                    .map(|p| p.value)
+                    .collect::<Vec<usize>>()
+                    .try_into()
+                    .unwrap();
+                if x < y {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketType::EqualThan => {
+                let [x, y]: [usize; 2] = packet
+                    .subpacket
+                    .iter()
+                    .map(solve_packet)
+                    .map(|p| p.value)
+                    .collect::<Vec<usize>>()
+                    .try_into()
+                    .unwrap();
+                if x == y {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketType::Unknown => unreachable!(),
+        },
+        type_id: PacketType::Literal,
+        ..Default::default()
+    }
+}
+
+fn part2() {
+    let bits: Vec<char> = std::fs::read_to_string("input")
+        .unwrap()
+        .chars()
+        .map(to_binary)
+        .map(str::chars)
+        .flatten()
+        .collect();
+    let packet = parse_packet(&bits);
+    let res = solve_packet(&packet);
+    println!("part2 {}", res.value);
+}
 
 fn main() {
     part1();
