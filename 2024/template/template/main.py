3e9e180e2e18317878652b2ccccaa9b959c25fbb
Lucas Lima <lucasflima25@gmail.com>
2024-12-01 22:24:49 -0300

    Uncomenting part2 from template

--- 2024/template/template/main.py
+++ 2024/template/template/main.py
@@ -9,7 +9,7 @@ def main():
     #with open("input.txt") as input:
         input = input.read()
     if ret := part1(input): return ret
-    #if ret := part2(input): return ret
+    if ret := part2(input): return ret
 
 if __name__ == "__main__":
     raise SystemExit(main())
