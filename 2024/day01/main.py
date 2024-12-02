f241eaf0af70f684baad887535e41c84737ca352
Lucas Lima <lucasflima25@gmail.com>
2024-12-01 22:04:16 -0300

    Fix template

--- 2024/day01/main.py
+++ 2024/day01/main.py
@@ -1,23 +1,12 @@
-import argparse
-import os
-import os.path
+def part1():
+    return 0
+
+def part2():
+    return 0
 
 def main():
-    parser = argparse.ArgumentParser(
-                    prog='Create day template',
-                    description='Creates template for given day of Advent of Code.',)
-    parser.add_argument('day')
-
-    args = parser.parse_args()
-    day = int(args.day)
-    day_folder = f"day{day:02}"
-
-    os.makedirs(day_folder, exist_ok=True)
-    with open(os.path.join("template", "main.py")) as template, \
-         open(os.path.join(day_folder, "main.py"), "w") as newfile:
-        newfile.write(template.read())
-
-    return 0
+    if ret := part1(): return ret
+    #if ret := part2(): return ret
 
 if __name__ == "__main__":
     raise SystemExit(main())
--- 2024/template/main.py
+++ 2024/template/main.py
@@ -9,16 +9,18 @@ def main():
     parser.add_argument('day')
 
     args = parser.parse_args()
+
     day_str = args.day
     if not day_str.isdigit():
         parser.print_usage()
         print("ERROR: day is not a number.")
         return 1
+    
     day = int(day_str) 
     day_folder = f"day{day:02}"
 
     os.makedirs(day_folder, exist_ok=True)
-    with open(os.path.join("template", "main.py")) as template, \
+    with open(os.path.join(os.path.dirname(__file__), "template", "main.py")) as template, \
          open(os.path.join(day_folder, "main.py"), "w") as newfile:
         newfile.write(template.read())
 
