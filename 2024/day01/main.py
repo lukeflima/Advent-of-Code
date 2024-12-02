5b55517cff392de4bd849deef949fa93e4835428
Lucas Lima <lucasflima25@gmail.com>
2024-12-01 22:25:14 -0300

    Day 1: Part 2

--- 2024/day01/main.py
+++ 2024/day01/main.py
@@ -1,4 +1,4 @@
-def part1(input):
+def get_lists(input):
     l1 = []
     l2 = []
     lines = input.split("\n")
@@ -6,14 +6,26 @@ def part1(input):
         [i, j] = line.split()
         l1.append(int(i))
         l2.append(int(j))
+    return l1, l2
+
+def part1(input):
+    l1, l2 = get_lists(input)
     
     l1.sort()
     l2.sort()
-    print("Part 1: ", sum(abs(i - j) for i, j in zip(l1, l2)))
+    print("Part 1:", sum(abs(i - j) for i, j in zip(l1, l2)))
 
     return 0
 
 def part2(input):
+    l1, l2 = get_lists(input)
+
+    s = 0
+    for i in l1:
+        s += i * sum(1 for j in l2 if i == j)
+    
+    print("Part 2:", s)
+
     return 0
 
 def main():
@@ -21,7 +33,7 @@ def main():
     with open("input.txt") as input:
         input = input.read()
     if ret := part1(input): return ret
-    #if ret := part2(input): return ret
+    if ret := part2(input): return ret
 
 if __name__ == "__main__":
     raise SystemExit(main())
