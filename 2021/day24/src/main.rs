d25abd0f4c1fd1d8310d558152d62888248159e2
Lucas Lima <lucasflima25@gmail.com>
2025-01-03 19:33:50 -0300

    [2021] Day 24

---
+++ 2021/day24/src/main.rs
@@ -0,0 +1,12 @@
+fn part1() {
+    let input = std::fs::read_to_string("input").unwrap().trim().to_string();
+}
+
+fn part2() {
+    let input = std::fs::read_to_string("input").unwrap().trim().to_string();
+}
+
+fn main() {
+    part1();
+    part2();
+}
