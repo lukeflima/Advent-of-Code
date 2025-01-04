b5fba978dff36749e1cbdd99b19d2a71b07d2e91
Lucas Lima <lucasflima25@gmail.com>
2025-01-04 11:29:23 -0300

    [2022] Template: increase file read max_bytes

--- 2022/template/src/template/src/main.zig
+++ 2022/template/src/template/src/main.zig
@@ -5,7 +5,7 @@ const fs = std.fs;
 fn read_input(input_file_name: []const u8, allocator: Allocator) ![]u8 {
     const input_file = try fs.cwd().openFile(input_file_name, .{});
     defer input_file.close();
-    return input_file.readToEndAlloc(allocator, 10000);
+    return input_file.readToEndAlloc(allocator, 4 * 1024 * 1024);
 }
 
 fn part1() !void {
