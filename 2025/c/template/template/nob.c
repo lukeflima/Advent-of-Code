b1c00cd77a356b61b1f7f35f2a4fa2174f5309d5
Lucas Lima <lucasflima25@gmail.com>
2026-01-08 12:35:34 -0300

    [2025] c template

--- 2025/c/template/template/nob.c
+++ 2025/c/template/template/nob.c
@@ -2,6 +2,7 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
+// gcc -o build/nob nob.c
 int main(int argc, char **argv) {
     GO_REBUILD_URSELF(argc, argv);
     
@@ -10,7 +11,8 @@ int main(int argc, char **argv) {
     cmd_append(&cmd, "cc");
     cmd_append(&cmd, "-Wall",  "-Wextra");
     cmd_append(&cmd, "-ggdb");
-    cmd_append(&cmd, "-o", "main", "main.c");
+    cmd_append(&cmd, "-o", "build/main", "main.c");
+    cmd_append(&cmd, "-lm");
     
     if (!cmd_run(&cmd)) return 1;
 
