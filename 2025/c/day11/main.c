96610b350b9e7c3c04319811b41501175dfaa195
Lucas Lima <lucasflima25@gmail.com>
2026-04-06 14:45:20 -0300

    [2025] using sv instead of id

--- 2025/c/day11/main.c
+++ 2025/c/day11/main.c
@@ -9,63 +9,54 @@
 #include "ht.h"
 
 typedef struct {
-    uint64_t* items;
+    String_View* items;
     size_t count;
     size_t capacity;
 } DeviceList;
 
-typedef Ht(uint64_t, DeviceList) Devices;
+typedef Ht(String_View, DeviceList) Devices;
 
-uint64_t name_to_id(Nob_String_View sv) {
-    assert(sv.count == 3);
-    return sv.data[0] + sv.data[1]*26 + sv.data[2]*26*26;
-}
+size_t paths(String_View cur, String_View target, Devices *devices) {
+    if(sv_eq(cur, target)) return 1;
 
-size_t paths(uint64_t cur, uint64_t target, Devices *devices) {
-    if(cur == target) return 1;
-    
     size_t res = 0;
-
     DeviceList *list =  ht_find(devices, cur);
     assert(list);
     for(size_t i = 0; i < list->count; i++) {
-        res += paths(list->items[i], target,devices);
+        res += paths(list->items[i], target, devices);
     }
 
     return res;
 }
 
 void part1(Nob_String_View input) {
-    Devices devices = {0};
+    Devices devices = {.hasheq = ht_sv_hasheq};
     while(input.count > 0) {
         Nob_String_View line = nob_sv_chop_by_delim(&input, '\n');
         Nob_String_View device_name = nob_sv_chop_by_delim(&line, ':');
         line = nob_sv_trim(line);
 
         DeviceList device_list = {0};
-        uint64_t device_id = name_to_id(device_name);
         while(line.count > 0) {
-            Nob_String_View num_sv = nob_sv_chop_by_delim(&line, ' ');
-            uint64_t id = name_to_id(num_sv);
-            nob_da_append(&device_list, id);
+            nob_da_append(&device_list, nob_sv_chop_by_delim(&line, ' '));
         }
-        *ht_put(&devices, device_id) = device_list;
+        *ht_put(&devices, device_name) = device_list;
     }
 
-    uint64_t start = name_to_id(nob_sv_from_cstr("you"));
-    uint64_t finish = name_to_id(nob_sv_from_cstr("out"));
+    String_View start = nob_sv_from_cstr("you");
+    String_View finish = nob_sv_from_cstr("out");
 
     size_t count = paths(start, finish, &devices);
 
     printf("Part 1: %zu\n", count);
 }
 
-static uint64_t OUT;
-static uint64_t FFT;
-static uint64_t DAC;
+static String_View OUT;
+static String_View FFT;
+static String_View DAC;
 
 typedef struct {
-    uint64_t cur;
+    String_View cur;
     bool found_fft;
     bool found_dac;
 } State;
@@ -76,7 +67,7 @@ size_t dfs(State state, Devices *devices, Cache *cache) {
     size_t *cached = ht_find(cache, state);
     if(cached) return *cached;
 
-    if(state.cur == OUT) {
+    if(sv_eq(state.cur, OUT)) {
         if(state.found_fft && state.found_dac) return 1;
         return 0;
     }
@@ -85,8 +76,8 @@ size_t dfs(State state, Devices *devices, Cache *cache) {
     DeviceList *list = ht_find(devices, state.cur); 
     assert(list);
     for(size_t i = 0; i < list->count; i++) {
-        uint64_t next = list->items[i];
-        State next_state = {next, state.found_fft || next == FFT, state.found_dac || next == DAC};
+        String_View next = list->items[i];
+        State next_state = {next, state.found_fft || sv_eq(next, FFT), state.found_dac || sv_eq(next, DAC)};
         total += dfs(next_state, devices, cache);
     }
 
@@ -95,29 +86,26 @@ size_t dfs(State state, Devices *devices, Cache *cache) {
 
 
 void part2(Nob_String_View input) {
-    Devices devices = {0};
+    Devices devices = {.hasheq = ht_sv_hasheq};
     while(input.count > 0) {
         Nob_String_View line = nob_sv_chop_by_delim(&input, '\n');
         Nob_String_View device_name = nob_sv_chop_by_delim(&line, ':');
         line = nob_sv_trim(line);
 
         DeviceList device_list = {0};
-        uint64_t device_id = name_to_id(device_name);
         while(line.count > 0) {
-            Nob_String_View num_sv = nob_sv_chop_by_delim(&line, ' ');
-            uint64_t id = name_to_id(num_sv);
-            nob_da_append(&device_list, id);
+            nob_da_append(&device_list, nob_sv_chop_by_delim(&line, ' '));
         }
-        *ht_put(&devices, device_id) = device_list;
+        *ht_put(&devices, device_name) = device_list;
     }
 
 
-    OUT = name_to_id(nob_sv_from_cstr("out"));
-    FFT = name_to_id(nob_sv_from_cstr("fft"));
-    DAC = name_to_id(nob_sv_from_cstr("dac"));
+    OUT = nob_sv_from_cstr("out");
+    FFT = nob_sv_from_cstr("fft");
+    DAC = nob_sv_from_cstr("dac");
 
     Cache cache = {0};
-    State start_state = {name_to_id(nob_sv_from_cstr("svr")), false, false};
+    State start_state = {nob_sv_from_cstr("svr"), false, false};
     uint64_t res = dfs(start_state, &devices, &cache);
 
     printf("Part 2: %zu\n", res);
@@ -136,7 +124,7 @@ int main(int argc, char **argv) {
     nob_read_entire_file(file_path, &input_sb);
     Nob_String_View input = nob_sv_from_parts(input_sb.items, input_sb.count);
     input = nob_sv_trim(input);
-    
+
     part1(input);
     part2(input);
 
